a9b63d12ff76ed88346aad5ad24076b6aeddd27d
headsin <headsin.get@gmail.com>
2025-08-11 10:47:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -340,7 +340,7 @@ def send_newsletter_via_heads_in(html_template):
     payload = {
         "template": html_template,
         "password": HEADSIN_PASSWORD,
-        "sendTo": [TO_EMAIL],
+        "sendTo": [],
         "placeholders": ["{{unsubscribeUrl}}"]
     }
 
